2d5ad42f674a5ab8e41d9fb5f6908926e5218b51
aye <chanmoe.aye@my.jcu.edu.au>
2024-10-06 15:49:26 +0800

    Add the answers

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+# When will a ValueError occur?
+# It happens when either the numerator or denominator is Zero.
+
+# When will a ZeroDivisionError occur?
+# It happens when the denominator is zero
+
+# Could you change the code to avoid the possibility of a ZeroDivisionError?
+# Yes, by adding error checking code.
