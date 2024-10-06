1ef58e183de31127b504b672cc7ddc6eac349d57
aye <chanmoe.aye@my.jcu.edu.au>
2024-10-06 15:12:24 +0800

    write the answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,26 @@
+import random
+
+# What did you see on line 1?
+# It prints a random integer between 5 and 20 inclusive.
+
+# What was the smallest number you could have seen, what was the largest?
+#The smallest number I have is 5 and the largest is 20
+
+# What did you see on line 2?
+# The code produces the numbers between 3 to 10 randomly in the step of 2. However, 10 cannot be produce as the step is 2.
+
+# What was the smallest number you could have seen, what was the largest?
+#The smallest number is 3 and the biggest is 9.
+
+# Could line 2 have produced a 4?
+# No.
+
+# What did you see on line 3?
+# Random decimal numbers between 2.5 and 5.5.
+
+# What was the smallest number you could have seen, what was the largest?
+# Smallest number is 2.5 and the biggest is 5.5.
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+random.randint
