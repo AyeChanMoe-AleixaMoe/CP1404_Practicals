ed3e4ae72c7506bd853a28a28fee06c80867926e
aye <chanmoe.aye@my.jcu.edu.au>
2024-11-10 18:24:43 +0800

    Complete first part of More Guitars exercise

---
+++ prac_07/guitar.py
@@ -0,0 +1,14 @@
+class Guitar:
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def get_age(self):
+        return 2022 - self.year  # Change to current year if needed
+
+    def is_vintage(self):
+        return self.get_age() >= 50
+
+    def __lt__(self, other):
+        return self.year < other.year
